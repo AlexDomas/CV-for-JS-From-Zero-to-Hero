5da98940479b96282690fc42b20da81c62741192
AlexDomas <aleksej.domas@mail.ru>
2023-01-11 22:56:21 +0300

    create folders for images + change main elements

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ function showMenu(){
 function hideMenu(){
     navLinks.style.right = "-200px";
 }
+
+var counter = 2;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 4){
+        counter = 1;
+    }
+} , 5000);
